8b851ac5ff5841c0a862f792487c9f0303d2ccf0
StevenJV <steven@vorefamily.net>
2019-03-04 16:25:53 -0500

    Add project files.

---
+++ jobjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jobjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
