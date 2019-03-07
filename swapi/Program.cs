cdf9ebc327a2724cff71cc956742d0ce145de107
StevenJV <steven@vorefamily.net>
2019-03-07 10:57:09 -0500

    calls Star Wars API for data

--- swapi/Program.cs
+++ swapi/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace swapi
 {
     class Program
     {
-        static void Main(string[] args)
+        static string _url = "http://swapi.co/api/";
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Galaxy!");
+            string planets = GetPlanet(1);
+            Console.WriteLine(planets);
+
+            Console.ReadKey();
+        }
+
+
+        public static string GetPlanet(int planetId)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                string fullUrl = _url + "planets/" + planetId;
+                var response = httpClient.GetStringAsync(new Uri(fullUrl)).Result;
+                return response;
+            }
+        }
+        public static string GetPlanet()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                string fullUrl = _url + "planets/";
+                var response = httpClient.GetStringAsync(new Uri(fullUrl)).Result;
+                return response;
+            }
         }
     }
 }
