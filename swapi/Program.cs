d4d6d154c2a2ba515f0ed97ef958304ff315251d
StevenJV <steven@vorefamily.net>
2019-03-13 15:30:20 -0400

    fetches film title for each planet seen.

--- swapi/Program.cs
+++ swapi/Program.cs
@@ -5,42 +5,72 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json.Linq;
 
-namespace swapi
+namespace StarWarsAPI
 {
-    class Program
+    public class Program
     {
-        static string _url = "http://swapi.co/api/";
+        private const string Url = "http://swapi.co/api/";
+        private static readonly HttpClient HttpClient = new HttpClient();
 
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello Galaxy!");
-            var planetData = JObject.Parse(GetPlanet());
-            var planetNames = from p in planetData["results"] select (string) p["name"]; // ok for GetPlanet(), but not for GetPlanet(1)
-            foreach(string name in planetNames) Console.WriteLine(name);
+            var countofplanets = GetNumberOfPlanets();
+            Console.WriteLine($"Hello Galaxy! There are {countofplanets} planets in the Star Wars universe. These have been seen in the movies:");
+            for (var planetNumber = 1;
+                planetNumber <= countofplanets;
+                planetNumber++)
+            {
+                var planetInfo = JObject.Parse(GetPlanet(planetNumber));
+                var planetName = planetInfo.Value<string>("name");
+                var planetTerrain = planetInfo.Value<string>("terrain");
+
+                var planetFilms = planetInfo["films"].Values<string>().ToList();
+                if (!planetFilms.Any()) continue;
+                Console.WriteLine($"{planetName} ({planetTerrain})");
+                Console.WriteLine(" seen in:");
+                foreach (var film in planetFilms)
+                {
+                    var uriAddress1 = new Uri(film);
+                    if (!int.TryParse(uriAddress1.Segments[uriAddress1.Segments.Length - 1].TrimEnd('/'),
+                        out var filmNumber)) continue;
+                    var filmInfo = JObject.Parse(GetFilm(filmNumber));
+                    var filmName = filmInfo.Value<string>("title");
+                    Console.WriteLine($"  {filmName}");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("No Bothans died to bring us this information.");
             Console.ReadKey();
         }
 
+        private static string GetFilm(int filmNumber)
+        {
+            HttpClient.DefaultRequestHeaders.Accept.Clear();
+            HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var fullUrl = Url + "films/" + filmNumber;
+            var response = HttpClient.GetStringAsync(new Uri(fullUrl)).Result;
+            return response;
+        }
 
         public static string GetPlanet(int planetId)
         {
-            using (var httpClient = new HttpClient())
-            {
-                httpClient.DefaultRequestHeaders.Accept.Clear();
-                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                string fullUrl = _url + "planets/" + planetId;
-                var response = httpClient.GetStringAsync(new Uri(fullUrl)).Result;
-                return response;
-            }
+            HttpClient.DefaultRequestHeaders.Accept.Clear();
+            HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var fullUrl = Url + "planets/" + planetId;
+            var response = HttpClient.GetStringAsync(new Uri(fullUrl)).Result;
+            return response;
         }
-        public static string GetPlanet()
+
+        public static int GetNumberOfPlanets()
         {
-            using (var httpClient = new HttpClient())
-            {
-                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                string fullUrl = _url + "planets/";
-                var response = httpClient.GetStringAsync(new Uri(fullUrl)).Result;
-                return response;
-            }
+            HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var fullUrl = Url + "planets/";
+            var response = HttpClient.GetStringAsync(new Uri(fullUrl)).Result;
+            var data = JObject.Parse(response);
+            var numberOfPlanets = (int) data.Children().First();
+            return numberOfPlanets;
         }
     }
 }
