33a57c8f05566544948b37908648fc19f6bc2cba
StevenJV <steven@vorefamily.net>
2019-03-14 14:57:11 -0400

    R# cleanup and reformat

--- jobjects/Program.cs
+++ jobjects/Program.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json.Linq;
 
-namespace jobjects
+namespace caseSections
 {
-    class Program
+    internal class Program
     {
-
         private static readonly string _getResponseText = @"{
   ""statusCode"": 200,
   ""data"": {
@@ -69,22 +67,26 @@ namespace jobjects
 
         private static void Main(string[] args)
         {
-            string _testSectionName = "Location";
-            string _testInsightsFieldName = "InsightsFieldName999";
+            var _testSectionName = "Location";
+            var _testInsightsFieldName = "InsightsFieldName999";
 
             var dashObj = JObject.Parse(_getResponseText);
 
-            Array sectionNames = GetArrayOfCaseSections(dashObj);
+            var sectionNames = GetArrayOfCaseSections(dashObj);
             Console.WriteLine($"there were {sectionNames.Length} sections found.");
 
-            int foundSection = Array.IndexOf(sectionNames, _testSectionName);
-            if (foundSection >= 0) Console.WriteLine($"section `{_testSectionName}` found, in caseSection [{foundSection}]"); //Assert.True(intTestSectionFound, $"{_testSectionName} section not found");
+            var foundSection = Array.IndexOf(sectionNames, _testSectionName);
+            if (foundSection >= 0)
+                Console.WriteLine(
+                    $"section `{_testSectionName}` found, in caseSection [{foundSection}]"); //Assert.True(intTestSectionFound, $"{_testSectionName} section not found");
 
             var itemsInSection = dashObj["data"]["caseSections"][foundSection]["customFieldItems"]
                 .Values<string>("insightsFieldName");
             var chosenItem = itemsInSection.SingleOrDefault(iis => iis.Contains(_testInsightsFieldName));
 
-            if (String.Equals(chosenItem, _testInsightsFieldName)) Console.WriteLine($"insightsFieldName `{_testInsightsFieldName}` found"); //Assert.True(insightsFieldNameFound, $"{_testInsightsFieldName} not found");
+            if (string.Equals(chosenItem, _testInsightsFieldName))
+                Console.WriteLine(
+                    $"insightsFieldName `{_testInsightsFieldName}` found"); //Assert.True(insightsFieldNameFound, $"{_testInsightsFieldName} not found");
 
             Console.ReadKey();
         }
